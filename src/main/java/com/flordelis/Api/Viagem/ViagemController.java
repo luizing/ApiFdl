07d4f9e77a8e47112ab7399b289bbf197b805ba0
Luiz <luizeduardo2099@gmail.com>
2025-09-13 16:01:14 -0300

    Viagem getById() implementado

--- src/main/java/com/flordelis/Api/Viagem/ViagemController.java
+++ src/main/java/com/flordelis/Api/Viagem/ViagemController.java
@@ -1,7 +1,9 @@
 package com.flordelis.Api.Viagem;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/viagem")
@@ -16,6 +18,9 @@ public class ViagemController {
 
     @GetMapping
     public List<ViagemModel> getAll(){return viagemService.getAll();}
+
+    @GetMapping("/{id}")
+    public Optional<ViagemModel> getById(@PathVariable("id")Long id){return viagemService.getById(id);}
 
     @PostMapping
     public ViagemModel create(@RequestBody ViagemModel viagem) {return viagemService.create(viagem);}
--- src/main/java/com/flordelis/Api/Viagem/ViagemService.java
+++ src/main/java/com/flordelis/Api/Viagem/ViagemService.java
@@ -3,6 +3,7 @@ package com.flordelis.Api.Viagem;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ViagemService {
@@ -15,6 +16,9 @@ public class ViagemService {
 
     //Listar todas as viagens
     public List<ViagemModel> getAll(){return viagemRepository.findAll();}
+
+    //Listar viagem por id
+    public Optional<ViagemModel> getById(Long id){return viagemRepository.findById(id);}
 
     //Adicionar Viagem
     public ViagemModel create(ViagemModel viagem){return viagemRepository.save(viagem);}
