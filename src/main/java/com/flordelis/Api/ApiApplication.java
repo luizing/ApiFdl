fcd0bdbb8b09673f5e18c952b85a0174bba10b0f
Luiz <luizeduardo2099@gmail.com>
2025-09-19 17:39:55 -0300

    feat: #17 - sistema de log implementado

--- src/main/java/com/flordelis/Api/ApiApplication.java
+++ src/main/java/com/flordelis/Api/ApiApplication.java
@@ -1,13 +1,18 @@
 package com.flordelis.Api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
+    private static Logger log = LoggerFactory.getLogger(ApiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+        SpringApplication.run(ApiApplication.class, args);
+	    log.info("Servidor Inicializado");
+    }
 
 }
