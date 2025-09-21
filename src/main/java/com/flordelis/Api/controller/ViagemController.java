01b9c6c8cc786ad71d95724c29a35b8a7e069bb5
Luiz <luizeduardo2099@gmail.com>
2025-09-21 14:20:42 -0300

    feat: implementado logs padronizados nos endpoints de viagemController.

--- src/main/java/com/flordelis/Api/controller/ViagemController.java
+++ src/main/java/com/flordelis/Api/controller/ViagemController.java
@@ -4,6 +4,7 @@ import com.flordelis.Api.dto.FinalizarViagemDTO;
 import com.flordelis.Api.model.ViagemModel;
 import com.flordelis.Api.service.ViagemService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/viagem")
+@Slf4j
 public class ViagemController {
 
 
@@ -25,12 +27,16 @@ public class ViagemController {
 
     @GetMapping
     public ResponseEntity<List<ViagemModel>> getAll(){
+        log.info("GET /viagem -> buscando todas as viagens");
         List<ViagemModel> viagens = viagemService.getAll();
+        log.info("GET /viagem -> retorno {} viagens",viagens.size());
         return ResponseEntity.ok(viagens);
     }
 
+    // Consertar log e retorno de erro
     @GetMapping("/{id}")
     public ResponseEntity<ViagemModel> getById(@PathVariable("id")Long id){
+        log.info("Viagem Request findById {}",id);
         Optional<ViagemModel> viagem = viagemService.getById(id);
         return viagem.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -38,31 +44,41 @@ public class ViagemController {
 
     @GetMapping("/abertas")
     public ResponseEntity<List<ViagemModel>> getByAbertas(){
+        log.info("GET /viagem/abertas -> buscando viagens em aberto");
         List<ViagemModel> viagens = viagemService.findByFinalizada(false);
+        log.info("GET /viagem/abertas -> retorno {} viagens",viagens.size());
         return ResponseEntity.ok(viagens);
     }
 
     @GetMapping("/finalizadas")
     public ResponseEntity<List<ViagemModel>> getByFinalizada(){
+        log.info("GET /viagem/finalizadas -> buscando viagens finalizadas");
         List<ViagemModel> viagens = viagemService.findByFinalizada(true);
+        log.info("GET /viagem/finalizadas -> retorno {} viagens",viagens.size());
         return ResponseEntity.ok(viagens);
     }
 
     @GetMapping("/data/{date}")
     public ResponseEntity<List<ViagemModel>> getByDate(@PathVariable("date")LocalDate date){
+        log.info("GET /viagem/data/{date} -> buscando viagens pela data {}",date);
         List<ViagemModel> viagens = viagemService.findByData(date);
+        log.info("GET /viagem/data/{date} -> retorno {} viagens",viagens.size());
         return ResponseEntity.ok(viagens);
     }
 
     @PostMapping
     public ResponseEntity<ViagemModel> create(@Valid @RequestBody CriarViagemDTO dto) {
+        log.info("POST /viagem -> criando viagem com dados: {}", dto);
         ViagemModel novaViagem = viagemService.create(dto.converter());
+        log.info("POST /viagem -> viagem criada com id = {}", novaViagem.getId());
         return new ResponseEntity<>(novaViagem,HttpStatus.CREATED);}
 
 
     @PatchMapping("/{id}")
     public ResponseEntity<ViagemModel> finalizar(@PathVariable Long id, @RequestBody FinalizarViagemDTO dto){
-    ViagemModel viagemFinalizada = viagemService.finalizar(id, dto);
-    return ResponseEntity.ok(viagemFinalizada);
+        log.info("PATCH /viagem/{} -> finalizando viagem", id);
+        ViagemModel viagemFinalizada = viagemService.finalizar(id, dto);
+        log.info("PATCH /viagem/{} -> viagem finalizada com sucesso", id);
+        return ResponseEntity.ok(viagemFinalizada);
     }
 }
